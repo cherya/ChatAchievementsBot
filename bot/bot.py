8e3b799400842316a794f16ff182c5c258bc8b7d
Илья Че <ilya@MacBook-Pro-Ila.local>
2016-12-02 16:01:11 +0300

    reset logging

--- bot/bot.py
+++ bot/bot.py
@@ -7,14 +7,17 @@ from pprint import pprint
 from datetime import datetime
 import telepot
 import logging
-logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(levelname)s:%(message)s')
+import os
+
+dir_name = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=dir_name+'/bot.log', level=logging.INFO, format='%(levelname)s:%(message)s')
 
 # for now let hardcode test chat and private
 # TEST_CHAT_ID = -1001072621302
 
 ADDMETO_CHAT_ID = -1001005702961
 
-chats = [ADDMETO_CHAT_ID, 29462028]
+chats = [29462028]
 
 bot = telepot.Bot(token=config['token'])
 
@@ -36,10 +39,10 @@ def handle_message(msg):
         message = Messages.create(id=msg['message_id'], message=msg, chat_id=chat_id, content_type=content_type)
         message.save()
         database.close()
-        username = msg['from']['username'] if 'username' in msg['from'] else msg['from']['id']
+        # username = msg['from']['username'] if 'username' in msg['from'] else msg['from']['id']
 
-        logging.INFO('{0} [{1}] {2}: {3}'.format(datetime.fromtimestamp(msg['date']).strftime('%Y-%m-%d %H:%M:%S'),
-                                                 username, content_type, msg[content_type]))
+        # logging.info('{0} [{1}] {2}: {3}'.format(datetime.fromtimestamp(msg['date']).strftime('%Y-%m-%d %H:%M:%S'),
+        #                                         username, content_type, msg[content_type]).encode('utf-8'))
 
         pprint(msg)
 
