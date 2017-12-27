7412eb9dd17b5e4677d3eb92b370f80f7f3efd74
jldeen <jessde@microsoft.com>
2017-12-27 13:34:42 +0300

    add 4th achievement level

--- bot/achievements.py
+++ bot/achievements.py
@@ -113,7 +113,7 @@ class AchievementBase:
 
 class Flooder(AchievementBase):
     name = 'Флудер'
-    levels = [100, 1000, 10000]
+    levels = [100, 1000, 10000, 100000]
 
     def check(self, msg, content_type, counters, cur_level):
         return content_type == 'text'
@@ -121,7 +121,7 @@ class Flooder(AchievementBase):
 
 class StickerSpammer(AchievementBase):
     name = 'Стикер-спаммер'
-    levels = [10, 50, 200]
+    levels = [10, 50, 200, 1000]
 
     def check(self, msg, content_type, counters, cur_level):
         return content_type == 'sticker'
@@ -129,7 +129,7 @@ class StickerSpammer(AchievementBase):
 
 class PontiusPilatus(AchievementBase):
     name = 'Понтий Пилат'
-    levels = [10, 100, 1000]
+    levels = [10, 100, 1000, 5000]
 
     def check(self, msg, content_type, counters, cur_level):
         return reply_from(AN, msg)
@@ -137,7 +137,7 @@ class PontiusPilatus(AchievementBase):
 
 class BackTo2007(AchievementBase):
     name = 'Назад в 2007'
-    levels = [2, 10, 50]
+    levels = [2, 10, 50, 200]
 
     def check(self, msg, content_type, counters, cur_level):
         if 'text' in msg:
@@ -150,7 +150,7 @@ class BackTo2007(AchievementBase):
 
 class WhyDoYouAsk(AchievementBase):
     name = 'А ви таки зачем интересуетесь?'
-    levels = [1, 10, 100]
+    levels = [1, 10, 100, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         count = 0
@@ -163,7 +163,7 @@ class WhyDoYouAsk(AchievementBase):
 
 class Dzhugashvili(AchievementBase):
     name = 'Джугашвили'
-    levels = [1, 5, 10]
+    levels = [1, 5, 10, 50]
 
     def update(self, msg, content_type, achievements_counters):
         if achievements_counters is None:
@@ -189,7 +189,7 @@ class Dzhugashvili(AchievementBase):
 
 class KernelPanic(AchievementBase):
     name = 'Kernel Panic'
-    levels = [1, 10, 100]
+    levels = [1, 10, 100, 1000]
 
     def check(self, msg, content_type, counters, cur_level):
         text = ''
@@ -209,7 +209,7 @@ class KernelPanic(AchievementBase):
 
 class FastestHandInTheWest(AchievementBase):
     name = 'Быстрая рука'
-    levels = [1, 10, 100]
+    levels = [1, 10, 100, 1000]
 
     def check(self, msg, content_type, counters, cur_level):
         if is_reply(msg):
@@ -218,28 +218,28 @@ class FastestHandInTheWest(AchievementBase):
 
 class Ametist(AchievementBase):
     name = 'Я у мамы аметист'
-    levels = [5, 20, 100]
+    levels = [5, 20, 100, 1000]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains_one_of(msg, ['бог', 'бога', ' богу', 'богом'])
 
 class PhotoReporter(AchievementBase):
     name = 'Фоторепортер'
-    levels = [5, 50, 500]
+    levels = [5, 50, 500, 2000]
 
     def check(self, msg, content_type, counters, cur_level):
         return content_type == 'photo'
 
 class TNN(AchievementBase):
     name = 'Девственник'
-    levels = [1, 5, 20]
+    levels = [1, 5, 20, 100]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains_one_of(msg, ['тнн', 'тян не нужны'])
 
 class Microblogger(AchievementBase):
     name = 'Микроблоггер'
-    levels = [1, 5, 20]
+    levels = [1, 5, 20, 100]
 
     def update(self, msg, content_type, achievements_counters):
         if achievements_counters is None:
@@ -265,21 +265,21 @@ class Microblogger(AchievementBase):
 
 class Nigilist(AchievementBase):
     name = 'Нигилист'
-    levels = [5, 20, 100]
+    levels = [5, 20, 100, 1000]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_equals(msg, 'нет') or msg_equals(msg, 'no')
 
 class Lolman(AchievementBase):
     name = 'Лiлка'
-    levels = [2, 15, 50]
+    levels = [2, 15, 50, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains_one_of(msg, ['лол','охлол','lol','лал'])
 
 class Zombie(AchievementBase):
     name = 'Зомби'
-    levels = [1, 3, 10]
+    levels = [1, 3, 10, 100]
 
     def check(self, msg, content_type, counters, cur_level):
         if is_reply(msg):
@@ -287,7 +287,7 @@ class Zombie(AchievementBase):
 
 class T800(AchievementBase):
     name = 'T800'
-    levels = [1, 2, 5]
+    levels = [1, 2, 5, 50]
 
     def check(self, msg, content_type, counters, cur_level):
         return is_forward_from(msg, ADDMETO_CHANNEL) or is_forward_from(msg, TECHSPARKS_CHANNEL)
@@ -305,7 +305,7 @@ class AddmetoReply(AchievementBase):
 
 class PlusPlus(AchievementBase):
     name = 'Инкремент'
-    levels = [3, 15, 50]
+    levels = [3, 15, 50, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_equals(msg, '+')
@@ -313,7 +313,7 @@ class PlusPlus(AchievementBase):
 
 class BG(AchievementBase):
     name = 'Мама, я не могу больше пить'
-    levels = [2, 10, 50]
+    levels = [2, 10, 50, 200]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains(msg, 'б-г')
@@ -321,7 +321,7 @@ class BG(AchievementBase):
 
 class SocialWhore(AchievementBase):
     name = 'Социоблядь'
-    levels = [20, 50, 100]
+    levels = [20, 50, 100, 300]
 
     def update(self, msg, content_type, achievements_counters):
         if achievements_counters is None:
@@ -345,7 +345,7 @@ class SocialWhore(AchievementBase):
 
 class Lisp(AchievementBase):
     name = '(list) ==>> NIL'
-    levels = [2, 10, 50]
+    levels = [2, 10, 50, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         text = get_msg_text(msg)
@@ -361,7 +361,7 @@ class Lisp(AchievementBase):
 # никсельпиксель
 class GoldenNicka(AchievementBase):
     name = 'Золотая Ника'
-    levels = [3, 15, 50]
+    levels = [3, 15, 50, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains(msg, 'хуемразь')
@@ -369,7 +369,7 @@ class GoldenNicka(AchievementBase):
 # паста про суп 
 class Dad(AchievementBase):
     name = 'Батя'
-    levels = [30, 70, 150]
+    levels = [30, 70, 150, 500]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains(msg, 'бля')
@@ -377,7 +377,7 @@ class Dad(AchievementBase):
 # упоротый папуг на арбузе 
 class JesusHearMe(AchievementBase):
     name = 'Есус ты миня слышеш'
-    levels = [5, 15, 70]
+    levels = [5, 15, 70, 300]
 
     def check(self, msg, content_type, counters, cur_level):
         return msg_contains_one_of(msg, ['папуг', 'попугай'])
