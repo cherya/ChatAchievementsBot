599f259dae964bbe424fa3a7ebe2fabd8571e191
jldeen <jessde@microsoft.com>
2017-12-31 18:54:02 +0300

    utf8 pprint

--- bot/bot.py
+++ bot/bot.py
@@ -19,7 +19,7 @@ bot = telepot.Bot(token=config['token'])
 
 class PrettyPrinter(pprint.PrettyPrinter):
     def format(self, object, context, maxlevels, level):
-        if isinstance(object, unicode):
+        if isinstance(object, str):
             return (object.encode('utf8'), True, False)
         return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
 
